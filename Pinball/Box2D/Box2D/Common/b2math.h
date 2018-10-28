4eddf3be9d04dfa28ba79bc2ede7007f578c14c3
lucho1 <luchosuaya@hotmail.com>
2018-10-28 14:04:05 +0100

    Update b2math.h

--- Pinball/Box2D/Box2D/Common/b2math.h
+++ Pinball/Box2D/Box2D/Common/b2math.h
@@ -57,6 +57,8 @@ struct b2Vec2
 
 	/// Construct using coordinates.
 	b2Vec2(float32 x, float32 y) : x(x), y(y) {}
+	
+	b2Vec2(int x, int y) : x(x), y(y) {}
 
 	/// Set this vector to all zeros.
 	void SetZero() { x = 0.0f; y = 0.0f; }
